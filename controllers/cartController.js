d293b06fc9881904e86a9c084e7b6611af7d5efa
malwinaniedziolka <mmalwina.niedziolka@gmail.com>
2025-05-14 19:03:36 +0200

    final update

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -4,10 +4,14 @@ const Cart = require("../models/Cart");
 const { STATUS_CODE } = require("../constants/statusCode");
 
 exports.addProductToCart = (request, response) => {
-  Product.add(request.body);
-  Cart.add(request.body.name);
-
-  response.status(STATUS_CODE.FOUND).redirect("/products/new");
+  Product.add(request.body)
+    .then(() => {
+      return Cart.add(request.body.name);
+    })
+    .then(() => {
+      response.status(STATUS_CODE.FOUND).redirect("/products/new");
+    })
+    .catch((error) => console.error(error));
 };
 
 exports.getProductsCount = () => {
